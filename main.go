384feac51e64428e193bb86ff019030162e039dd
Pablo Trinidad <pabloatp0204@gmail.com>
2019-07-01 22:08:51 -0500

    Careers fetched with their academic plans

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"regexp"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
@@ -23,9 +27,36 @@ var httpClient = &http.Client{
 }
 
 func main() {
+	// Match content inside parenthesis
+	re := regexp.MustCompile(`\([^)]+\)`)
+
+	// Fetch main index document
 	indexDoc := getDocument("docencia/horarios/indice")
+
+	// Obtain careers and their academic plans
 	indexDoc.Find("#info-contenido h2").Each(func(i int, s *goquery.Selection) {
-		fmt.Printf("%d) %s\n", i+1, s.Text())
+		// Degree name
+		degreeName := s.Text()
+		fmt.Printf("%d) %s\n", i+1, degreeName)
+
+		// Academic plans
+		s.Next().Find("a").Each(func(j int, t *goquery.Selection) {
+			academicPlan := re.FindString(t.Text())
+			academicPlan = strings.Title(academicPlan[1 : len(academicPlan)-1])
+
+			// Obtain plan ID from URL
+			ref, _ := t.Attr("href")
+			planURL, err := url.Parse(ref)
+			if err != nil {
+				log.Fatal(err)
+			}
+			urlComponents := strings.Split(planURL.Path, "/")
+			academicPlanID, err := strconv.Atoi(urlComponents[len(urlComponents)-1])
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("\t%d.%d) % s (with ID: %d)\n", i, j, academicPlan, academicPlanID)
+		})
 	})
 }
 
