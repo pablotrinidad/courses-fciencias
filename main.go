1c90aed40a4e64fd2baac0eb5adc80c77776072f
Pablo Trinidad <pabloatp0204@gmail.com>
2019-07-14 15:09:42 -0500

    Majors obtained

--- main.go
+++ main.go
@@ -4,8 +4,21 @@
 
 package main
 
-import "github.com/pablotrinidad/courses-fciencias/fciencias"
+import (
+	"fmt"
+
+	"github.com/pablotrinidad/courses-fciencias/fciencias"
+)
 
 func main() {
-	fciencias.GetMajors()
+	majors := fciencias.GetMajors()
+	for _, major := range majors {
+		fmt.Printf("%d) %s\n", major.ID, major.Name)
+		for _, plan := range major.AcademicPlans {
+			fmt.Printf(
+				"\t%d (ext: %d)\t%s\tyear: %d\n",
+				plan.ID, plan.ExternalID, plan.Name, plan.Year)
+		}
+	}
+
 }
