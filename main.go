f7e671f8292ce3107863d8ad92b51b6eba03aefe
Pablo Trinidad <pabloatp0204@gmail.com>
2019-07-14 16:07:53 -0500

    Gcloud deployment added

--- main.go
+++ main.go
@@ -1,24 +1,34 @@
 // Course FCiencias.
 // A web crawler made to download and store UNAM's Faculty of Science
-// courses schedules.
+// available courses schedules.
 
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 
 	"github.com/pablotrinidad/courses-fciencias/fciencias"
 )
 
 func main() {
-	majors := fciencias.GetMajors()
-	for _, major := range majors {
-		fmt.Printf("%d) %s\n", major.ID, major.Name)
-		for _, plan := range major.AcademicPlans {
-			fmt.Printf(
-				"\t%d (ext: %d)\t%s\tyear: %d\n",
-				plan.ID, plan.ExternalID, plan.Name, plan.Year)
-		}
+	http.HandleFunc("/", indexHandler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
 	}
 
+	log.Printf("Listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	majors := fciencias.GetMajors()
+	data, _ := json.MarshalIndent(majors, "", "    ")
+	fmt.Fprintf(w, "%s\n", data)
 }
